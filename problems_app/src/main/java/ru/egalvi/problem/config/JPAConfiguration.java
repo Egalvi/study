27220d31104be38820e67a4da51eb6f1fbfebe06
Egalvi <egalvi@mail.ru>
2014-09-12 13:34:37 +0400

    DB automatic creation was estabilished. It's not recommended for production

--- problems_app/src/main/java/ru/egalvi/problem/config/JPAConfiguration.java
+++ problems_app/src/main/java/ru/egalvi/problem/config/JPAConfiguration.java
@@ -43,7 +43,7 @@ public class JPAConfiguration {
     private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
     private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
 
-    private static final String PROPERTY_NAME_HIBERNATE_DDL_AUTO = "spring.jpa.hibernate.ddl-auto";
+    private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
     private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
     private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
     private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
@@ -79,12 +79,11 @@ public class JPAConfiguration {
         entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
 
         Properties jpaProterties = new Properties();
-        jpaProterties.put(PROPERTY_NAME_HIBERNATE_DDL_AUTO,
-                environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DDL_AUTO));
         jpaProterties.put(PROPERTY_NAME_HIBERNATE_DIALECT,
                 environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
         jpaProterties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL,
                 environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
+        jpaProterties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
         jpaProterties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY,
                 environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
         jpaProterties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL,
