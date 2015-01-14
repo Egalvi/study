0922d134e176d87a7d605f7fb5e284206244ea11
Egalvi <egalvi@mail.ru>
2015-01-14 19:00:03 +0300

    Output for disciplines is made as links

--- src/main/java/ru/egalvi/problem/web/rest/DisciplineRestService.java
+++ src/main/java/ru/egalvi/problem/web/rest/DisciplineRestService.java
@@ -2,7 +2,10 @@ package ru.egalvi.problem.web.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.egalvi.problem.core.domain.DisciplineDto;
 import ru.egalvi.problem.persistence.service.DisciplineService;
 
 import javax.annotation.Resource;
@@ -21,5 +24,11 @@ public class DisciplineRestService
     public String listAllProblems(Model model) {
         model.addAttribute("disciplines", disciplineService.findAll());
         return "discipline";
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String create(@ModelAttribute("discipline")DisciplineDto disciplineDto, Model model) {
+        model.addAttribute("foo", "You've asked for problem with id. Not implemented yet");
+        return "redirect:discipline";
     }
 }
