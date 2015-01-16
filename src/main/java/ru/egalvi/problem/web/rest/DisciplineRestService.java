c4d196c577bd29b72649694a8387dbbd01339e38
Egalvi <egalvi@mail.ru>
2015-01-16 15:45:04 +0300

    #1 Add and delete functions implemented for Discipline

--- src/main/java/ru/egalvi/problem/web/rest/DisciplineRestService.java
+++ src/main/java/ru/egalvi/problem/web/rest/DisciplineRestService.java
@@ -3,10 +3,12 @@ package ru.egalvi.problem.web.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.egalvi.problem.core.domain.DisciplineDto;
 import ru.egalvi.problem.persistence.service.DisciplineService;
+import ru.egalvi.problem.persistence.service.EntityNotFoundException;
 
 import javax.annotation.Resource;
 
@@ -21,14 +23,21 @@ public class DisciplineRestService
     private DisciplineService disciplineService;
 
     @RequestMapping("")
-    public String listAllProblems(Model model) {
+    public String listAll(Model model) {
         model.addAttribute("disciplines", disciplineService.findAll());
+        model.addAttribute("disciplineDto", new DisciplineDto());
         return "discipline";
     }
 
-    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+    public String delete(@PathVariable(value = "id") Long id, Model model) throws EntityNotFoundException{
+        disciplineService.delete(id);
+        return "redirect:/discipline";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String create(@ModelAttribute("discipline")DisciplineDto disciplineDto, Model model) {
-        model.addAttribute("foo", "You've asked for problem with id. Not implemented yet");
-        return "redirect:discipline";
+        disciplineService.create(disciplineDto);
+        return "redirect:/discipline";
     }
 }
